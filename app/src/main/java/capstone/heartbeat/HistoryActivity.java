217d6360b9660c941c5f013e4633cc13f423104f
Mark Anthony Torres <markanthonytorres30@gmail.com>
2017-11-30 07:02:51 +0800

    backend integration

--- app/src/main/java/capstone/heartbeat/HistoryActivity.java
+++ app/src/main/java/capstone/heartbeat/HistoryActivity.java
@@ -1,17 +1,149 @@
 package capstone.heartbeat;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class HistoryActivity extends AppCompatActivity {
+    private Button diabetes_t1no,diabetes_t1yes,famhistory_yes,famhistoy_no;
+    private Switch congestive,heartattack,valvular,irrefular,rheumatoid,chronic;
+    private int type1,cong,ha,val,irr,rhe,chr,fhcvd;
+    private SharedPreferences prefs = getSharedPreferences("values",MODE_PRIVATE);
+    private SharedPreferences.Editor editor = prefs.edit();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        diabetes_t1no = (Button)findViewById(R.id.btn_diabetes_no);
+        diabetes_t1yes = (Button)findViewById(R.id.btn_diabetes_yes);
+        famhistory_yes =(Button)findViewById(R.id.btn_famhistory_yes);
+        famhistoy_no = (Button)findViewById(R.id.btn_famhistory_no);
+        congestive = (Switch)findViewById(R.id.sw_congestive);
+        heartattack =(Switch)findViewById(R.id.sw_heartattack);
+        valvular = (Switch)findViewById(R.id.sw_valvular);
+        irrefular = (Switch)findViewById(R.id.sw_irregular);
+        rheumatoid =(Switch)findViewById(R.id.sw_rheumatoid);
+        chronic = (Switch)findViewById(R.id.sw_chronic);
+
+        diabetes_t1yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                type1 = 1;
+                editor.putInt("type1",type1);
+                editor.commit();
+            }
+        });
+        diabetes_t1no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                type1 = 0;
+                editor.putInt("type1",type1);
+                editor.commit();
+            }
+        });
+        famhistory_yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fhcvd = 1;
+                editor.putInt("fhcvd",fhcvd);
+                editor.commit();
+            }
+        });
+        famhistoy_no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fhcvd = 0;
+                editor.putInt("fhcvd",fhcvd);
+                editor.commit();
+            }
+        });
+        congestive.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
+                    cong = 1;
+                    editor.putInt("congestive",cong);
+                    editor.commit();
+                }else {
+                    cong = 0;
+                    editor.putInt("congestive",cong);
+                    editor.commit();
+                }
+            }
+        });
+        heartattack.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
+                    ha = 1;
+                    editor.putInt("heartattack",ha);
+                    editor.commit();
+                }else {ha =0;
+                    editor.putInt("heartattack",ha);
+                    editor.commit();}
+            }
+        });
+        valvular.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    val = 1;
+                    editor.putInt("valvular",val);
+                    editor.commit();
+                }else {val = 0;
+                    editor.putInt("valvular",val);
+                    editor.commit();}
+            }
+        });
+        irrefular.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
+                    irr=1;
+                    editor.putInt("irregular",irr);
+                    editor.commit();
+                }else {irr =0;
+                    editor.putInt("irregular",irr);
+                    editor.commit();}
+            }
+        });
+        rheumatoid.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
+                    rhe = 1;
+                    editor.putInt("rheumatoid",rhe);
+                    editor.commit();
+                }else {rhe =0;
+                    editor.putInt("rheumatoid",rhe);
+                    editor.commit();}
+            }
+        });
+        chronic.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
+                    chr =1;
+                    editor.putInt("chronic",chr);
+                    editor.commit();
+                }else {chr = 0;
+                    editor.putInt("chronic",chr);
+                    editor.commit();}
+            }
+        });
+
+
+
+
     }
 
     @Override
@@ -31,6 +163,7 @@ public class HistoryActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.next) {
+
             startActivity(new Intent(getApplicationContext(),RiskResultsActivity.class));
             return true;
         }
